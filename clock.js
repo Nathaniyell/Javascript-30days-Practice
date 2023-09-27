78c8f9df33f209abf7f9582e749f2cba3c73974a
Nathaniyell <essien.nathan@yahoo.com>
2023-09-28 00:36:59 +0100

    update the secondHand element

--- clock.js
+++ clock.js
@@ -1,7 +1,11 @@
+const secondHand = document.querySelector(".second-hand")
+
 function setDate() {
   const now = new Date(); //gets the currentTime
   const seconds = now.getSeconds();
-  const secondsInDegrees = (seconds / 60) * 360;
-  console.log();
+  const secondsInDegrees = ((seconds / 60) * 360) + 90
+  console.log(seconds);
+  secondHand.style.transform = `rotate(${secondsInDegrees}deg)`
+
 }
 setInterval(setDate, 1000); //The setInterval method helps you do something repeatedly. In this case, update the time after every sescond
