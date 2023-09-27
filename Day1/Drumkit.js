cce948bebae66786d4c0982d2190dd13e309bf89
Nathaniyell <essien.nathan@yahoo.com>
2023-09-27 23:50:01 +0100

    add code to select the audio element

--- Day1/Drumkit.js
+++ Day1/Drumkit.js
@@ -1,3 +1,7 @@
 window.addEventListener("keydown", (e) => {
-  console.log(e); // lists all the properties attributed to the key that was pressed
+  //console.log(e); //lists all the properties of the event object attributed to the key that was pressed
+  console.log(e.keyCode); // give the keycode property associated to the key that was pressed
+
+  //check for an element that has the keycode of 65
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) //using the attribute selector, you select the audio element that has a keycode of the key selected
 });
