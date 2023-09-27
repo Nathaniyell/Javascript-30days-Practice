13dcf03fc764ad9944186e7f1bea1ee4b7c866b8
Nathaniyell <essien.nathan@yahoo.com>
2023-09-28 00:31:56 +0100

    add logic to convert seconds to a value in degrees

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 function setDate() {
   const now = new Date(); //gets the currentTime
   const seconds = now.getSeconds();
-  
+  const secondsInDegrees = (seconds / 60) * 360;
+  console.log();
 }
 setInterval(setDate, 1000); //The setInterval method helps you do something repeatedly. In this case, update the time after every sescond
