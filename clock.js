d56cb53df5f0726f7548633606dd5e84722cd8d8
Nathaniyell <essien.nathan@yahoo.com>
2023-09-28 00:44:24 +0100

    add functionality to the minuteHand

--- clock.js
+++ clock.js
@@ -1,11 +1,21 @@
-const secondHand = document.querySelector(".second-hand")
+const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date(); //gets the currentTime
   const seconds = now.getSeconds();
-  const secondsInDegrees = ((seconds / 60) * 360) + 90
-  console.log(seconds);
-  secondHand.style.transform = `rotate(${secondsInDegrees}deg)`
+  const secondsInDegrees = (seconds / 60) * 360 + 90;
+  //console.log(seconds);
+  secondHand.style.transform = `rotate(${secondsInDegrees}deg)`;
 
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minsHand.style.transform=`rotate(${minsDegrees}deg)`;
+
+ 
 }
+
+
+
 setInterval(setDate, 1000); //The setInterval method helps you do something repeatedly. In this case, update the time after every sescond
