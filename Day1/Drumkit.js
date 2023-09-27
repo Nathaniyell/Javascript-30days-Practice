0455dc63f8869a330113a8d92e33a411b8527953
Nathaniyell <essien.nathan@yahoo.com>
2023-09-27 23:57:39 +0100

    add key variable to select all keys

--- Day1/Drumkit.js
+++ Day1/Drumkit.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", (e) => {
   //console.log(e); //lists all the properties of the event object attributed to the key that was pressed
-  console.log(e.keyCode); // give the keycode property associated to the key that was pressed
+  //console.log(e.keyCode); // give the keycode property associated to the key that was pressed
 
   //check for an element that has the keycode of 65
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //using the attribute selector, you select the audio element that has a keycode of the key selected
@@ -8,4 +8,8 @@ window.addEventListener("keydown", (e) => {
   if (!audio) return; //if the key pressed is not attributed to any audio element then return
   audio.currentTime = 0; // rewinds to the start incase the key is pressed more than once
   audio.play(); //plays the sound associated with the key that was pressed
+
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //selects all the elements with a class of key which has the data attribute specified
+  console.log(key);
+
 });
