109489de5c05c79d106ee228e99f13c4b68cc7e1
Nathaniyell <essien.nathan@yahoo.com>
2023-09-27 23:55:06 +0100

    add logic to rewind the sound to the start if pressed more than once

--- Day1/Drumkit.js
+++ Day1/Drumkit.js
@@ -6,5 +6,6 @@ window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //using the attribute selector, you select the audio element that has a keycode of the key selected
 
   if (!audio) return; //if the key pressed is not attributed to any audio element then return
-  audio.play() //plays the sound associated with the key that was pressed
+  audio.currentTime = 0; // rewinds to the start incase the key is pressed more than once
+  audio.play(); //plays the sound associated with the key that was pressed
 });
