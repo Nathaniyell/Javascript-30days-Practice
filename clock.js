9dba7e3952a4b7e45d7066b8f157455861d555d8
Nathaniyell <essien.nathan@yahoo.com>
2023-09-28 00:44:42 +0100

    add functionality to the hourHand

--- clock.js
+++ clock.js
@@ -13,7 +13,9 @@ function setDate() {
   const minsDegrees = (mins / 60) * 360 + 90;
   minsHand.style.transform=`rotate(${minsDegrees}deg)`;
 
- 
+  const hour = now.getHours;
+  const hourDegrees = (hour / 12) * 360 + 90;
+  hourHand.style.transform=`rotate(${hourDegrees}deg)`;
 }
 
 
