621cc2ef51595c617e4bf501967fa20ae86e0292
Nathaniyell <essien.nathan@yahoo.com>
2023-09-28 01:13:13 +0100

    add functionality to  update the variables value

--- Day3/index.js
+++ Day3/index.js
@@ -2,7 +2,12 @@ const inputs = document.querySelectorAll(".controls input");
 //querySelectorAll essentially returns something that looks like an array, called a NodeList. The difference between a nodeList an an array is that the nodeList has limited number of methods while an array has all the methods attributed to an array
 
 function handleUpdate() {
-  console.log(this.value);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  ); //selects the variable by going through the document domeNode
+  // console.log(this.value);
 }
 inputs.forEach((input) => input.addEventListener("change", handleUpdate));
 inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate)); //whenever the mousemoves the range bars, there should be an update
