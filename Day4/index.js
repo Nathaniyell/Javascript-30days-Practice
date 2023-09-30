e6a481be7903a745cd1b7db7ae27baacd34121b4
Nathaniyell <essien.nathan@yahoo.com>
2023-09-30 23:28:49 +0100

    begin challenge 5 of day 4

--- Day4/index.js
+++ Day4/index.js
@@ -91,7 +91,9 @@ const totalYears = inventors.reduce((total, inventor) => {
 console.log(totalYears);
 
 // 5. Sort the inventors by years lived
-
+const oldest = inventors.sort((big, small)=>{
+    
+})
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
