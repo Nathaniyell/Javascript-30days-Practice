77e9659374169aee91203aed84bae90bbdfbdcdb
Nathaniyell <essien.nathan@yahoo.com>
2023-10-01 21:07:44 +0100

    add eventListener for transition and click events

--- Day5/index.js
+++ Day5/index.js
@@ -1,2 +1,15 @@
-const panel = document.querySelectorAll(".panel");
-pan
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => panel.addEventListener("click", () => {
+    // console.log("hello")
+    panel.classList.toggle("open");
+  }));
+
+  panels.forEach(panel=>{
+    return panel.addEventListener("transitionend", (e)=>{
+        if(e.propertyName.includes("flex")){ //if the propertyname of the transition that ends is flex, it should toggle the class of open-active
+            panel.classList.toggle("open-active")
+
+        }
+    })
+  })
