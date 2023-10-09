f0b088cd8b08ee4efcd3da5cb451f72abdc0127c
Nathaniyell <essien.nathan@yahoo.com>
2023-10-09 09:36:08 +0100

    add code to find an id using .find() method

--- Day7/index.js
+++ Day7/index.js
@@ -41,6 +41,11 @@ console.log(allAdults) //.every() returns false if not all the items of the arra
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const findId = comments.find(comment=>comment.id===823423)
+  console.log(findId)
+//.find() - Instead of giving you a subset of the array just like filter would, it will just give you the item that you are looking for
+
+
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
