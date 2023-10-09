897e9f4b8830f807b228306c21445090d77bc957
Nathaniyell <essien.nathan@yahoo.com>
2023-10-09 10:42:09 +0100

    add function to draw on canvas

--- Day8/index.js
+++ Day8/index.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector("#draw");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = "#BADASS";
+ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 
@@ -11,9 +11,18 @@ let lastX = 0;
 let lastY = 0;
 
 function draw(e) {
-  if (!isDrawing) return; //this stops the function from running when the mouse hasn't been clicked on the canvas
+if (!isDrawing) return; //this stops the function from running when the mouse hasn't been clicked on the canvas
   console.log(e);
+  ctx.beginPath()
+//   start from
+  ctx.moveTo(lastX, lastY)
+//   go to
+  ctx.lineTo(e.offsetX, e.offsetY)
+//   do the actual drawing
+  ctx.stroke()
 }
-
-canvas.addEventListener("movemouse", draw);
 canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+canvas.addEventListener("mousemove", draw);
