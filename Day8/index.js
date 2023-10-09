f5096d2f31502b625719c7a1bd0d6d355f32caf7
Nathaniyell <essien.nathan@yahoo.com>
2023-10-09 09:59:29 +0100

    add variables to aid drawing on the canvas

--- Day8/index.js
+++ Day8/index.js
@@ -1,1 +1,19 @@
 const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADASS";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return; //this stops the function from running when the mouse hasn't been clicked on the canvas
+  console.log(e);
+}
+
+canvas.addEventListener("movemouse", draw);
+canvas.addEventListener("mousedown", () => (isDrawing = true));
