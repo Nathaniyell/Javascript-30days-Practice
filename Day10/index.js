201baf83b7232f7e959523dbd0fecd35f9875279
Nathaniyell <essien.nathan@yahoo.com>
2023-10-20 21:46:22 +0100

    day 10 completed

--- Day10/index.js
+++ Day10/index.js
@@ -1,11 +1,22 @@
-const checkboxes = document.querySelectorAll("input[type='checkbox']")
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
 let lastChecked;
-checkboxes.forEach(checkbox=>{
-    checkbox.addEventListener('click',(e)=>{
-        //check if the input element clicked had the shiftkey down
-        if(e.shiftKey && this.checked){
 
-        }
-console.log(e)
-    })
-})
+function handleChecked(e) {
+  //check if the input element clicked had the shiftkey down
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check inbetween!");
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  // console.log(e);
+}
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("click", handleChecked);
+});
