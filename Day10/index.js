80c6b746aa31b1269867dd1d8ea30eb2645f0981
Nathaniyell <essien.nathan@yahoo.com>
2023-10-20 21:27:47 +0100

    add if block to check status of checkbox

--- Day10/index.js
+++ Day10/index.js
@@ -1,7 +1,11 @@
 const checkboxes = document.querySelectorAll("input[type='checkbox']")
-
+let lastChecked;
 checkboxes.forEach(checkbox=>{
     checkbox.addEventListener('click',(e)=>{
+        //check if the input element clicked had the shiftkey down
+        if(e.shiftKey && this.checked){
+
+        }
 console.log(e)
     })
 })
