2241b0bf74ca57eff0c7528f2af06ad28f109cc6
Nathaniyell <essien.nathan@yahoo.com>
2023-10-21 12:57:04 +0100

    add function to handleProgress of the video

--- Day11/scripts.js
+++ Day11/scripts.js
@@ -26,19 +26,25 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate(){
-video[this.name] = this.value;
-/*
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  /*
 video["volume"] = value from the slider;
 video["playbackRate"] = value from the slider;
 the volume or the playbackRate property will be selected and updated dynamically whenever the sliders are used
 */
 }
 
+function handleProgress() {
+  const percentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percentage}%`
+}
+
 /*Hook up event listeners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateBtn);
 video.addEventListener("pause", updateBtn);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 
@@ -46,6 +52,6 @@ skipButtons.forEach((button) => {
   button.addEventListener("click", skip);
 });
 
-ranges.forEach(range=>{
-    range.addEventListener("change", handleRangeUpdate)
-})
+ranges.forEach((range) => {
+  range.addEventListener("change", handleRangeUpdate);
+});
