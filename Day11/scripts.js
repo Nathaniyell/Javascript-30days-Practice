66475442a789e6e3f0ba64b40551ab6c3cfd2fa1
Nathaniyell <essien.nathan@yahoo.com>
2023-10-21 12:38:40 +0100

    add skip forward and mbackward functionality

--- Day11/scripts.js
+++ Day11/scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelector("[data-skip]");
+const skipButtons = document.querySelectorAll("[data-skip]");
 const ranges = player.querySelector(".player__slider");
 
 /*Build functions */
@@ -15,15 +15,24 @@ function togglePlay() {
     video.pause();
   }
 }
-function updateBtn(){
-    const icon = this.paused ? '►' : '❚ ❚';
+function updateBtn() {
+  const icon = this.paused ? "►" : "❚ ❚";
   console.log(icon);
   toggle.textContent = icon;
 }
 
+function skip() {
+  console.log(this.dataset);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+pro
 /*Hook up event listeners */
-video.addEventListener("click", togglePlay)
-video.addEventListener("play", updateBtn)
-video.addEventListener("pause", updateBtn)
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateBtn);
+video.addEventListener("pause", updateBtn);
 
-toggle.addEventListener("click", togglePlay)
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => {
+  button.addEventListener("click", skip);
+});
