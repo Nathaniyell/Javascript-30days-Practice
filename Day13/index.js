73d4460a945e9be41212f5bd440706b1612cdd4d
Nathaniyell <essien.nathan@yahoo.com>
2023-10-27 23:39:39 +0100

    update Day13/index.js

--- Day13/index.js
+++ Day13/index.js
@@ -1,27 +1,30 @@
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  }
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-  const slideImages = document.querySelectorAll(".slide-in")
+const slideImages = document.querySelectorAll(".slide-in");
 
-  function checkslide(e){
-    slideImages.forEach(slideImage=>{
-        const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2 //half-way theough the image
-        
-        const imageBottom = slideImage.offsetTop + slideImage.height //bottom of the image
-    })
+function checkslide(e) {
+  slideImages.forEach((slideImage) => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - slideImage.height / 2; //half-way theough the image
 
-  }
+    const imageBottom = slideImage.offsetTop + slideImage.height; //bottom of the image
 
-  window.addEventListener("scroll", debounce(checkslide))
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+  });
+}
+
+window.addEventListener("scroll", debounce(checkslide));
