3d4ea65946fa3420addff6a5c299b4cad8e6b8a0
Nathaniyell <essien.nathan@yahoo.com>
2023-10-31 22:21:21 +0100

    update Day15/index.js

--- Day15/index.js
+++ Day15/index.js
@@ -15,5 +15,12 @@ const addItems = document.querySelector('.add-items');
     this.reset();
   }
   function populateList(plates = [], platesList) {
-
+    platesList.innerHTML = plates.map((plate, i) => {
+      return `
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+          <label for="item${i}">${plate.text}</label>
+        </li>
+      `;
+    }).join('');
   }
